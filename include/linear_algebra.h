76d8979886a612f96e6c820c4f2d519b5ed0b867
Jens Top <topjens@gmail.com>
2018-07-12 11:58:45 +0200

    Initial commit

---
+++ include/linear_algebra.h
@@ -0,0 +1,6 @@
+#ifndef _LINEAR_ALGEBRA_H__
+#define _LINEAR_ALGEBRA_H__
+
+#include <math.h>
+
+#endif /* _LINEAR_ALGEBRA_H__ */
---
+++ include/load_texture.h
@@ -0,0 +1,9 @@
+#ifndef _LOAD_TEXTURE_H__
+#define _LOAD_TEXTURE_H__
+
+#include <SDL/SDL_image.h>
+#include "glad/glad.h"
+
+int load_texture(const char *filename);
+
+#endif /* _LOAD_TEXTURE_H__ */
